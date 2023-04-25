249a2bc003cfe3361b694bcb94d084c15bd884fc
Patrik He <patrikswgit@gmail.com>
2023-04-25 13:20:55 +0200

    A very basic first React app with a stateful variable and onClick-event and changed to dark mode with bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+	const [msg, setMsg] = useState("Hi mom, I'm stateful")
+	const [count, setCount] = useState(0)
+
+	const handleButtonClick = () => {
+		setMsg("Bye mom")
+		setCount(count +1)
+	}
+
+
+	return (
+		<div className="App">
+			<h1>React Basic</h1>
+
+			<h2>{msg}</h2>
+
+			<p>You have clicked the button {count} times</p>
+
+			<button onClick={handleButtonClick} className="btn btn-success btn-lg">⇲me!</button>
+		</div>
+	)
+}
+
+export default App
