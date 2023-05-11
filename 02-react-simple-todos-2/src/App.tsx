c8edaa8a6f1263e5f9d323ee530fdddae3e6985a
Patrik He <patrikswgit@gmail.com>
2023-05-11 15:20:12 +0200

    moved todo counter to TodoCounter component

--- 02-react-simple-todos-2/src/App.tsx
+++ 02-react-simple-todos-2/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Todo, TodoList } from './assets/types'
 import TodoListItem from './assets/components/TodoListItem'
+import TodoCounter from './assets/components/TodoCounter'
 import './App.css'
 
 function App() {
@@ -55,42 +56,50 @@ function App() {
 
 	return (
 		<div className='App'>
-		<h1>React Todo</h1>
+			<h1>React Todo</h1>
 
-		<form onSubmit={handleSubmit} className="mb-3">
-			<div className="input-group mb-3">
-				<input
-				type="text"
-				className="form-control"
-				placeholder='Create Todo'
-				onChange={e => setNewTodoTitle(e.target.value)}
-				value={newTodoTitle}
-				/>
-				<button className="btn btn-success mb">Create</button>
-			</div>
-		</form>
+			<form onSubmit={handleSubmit} className="mb-3">
+				<div className="input-group mb-3">
+					<input
+					type="text"
+					className="form-control"
+					placeholder='Create Todo'
+					onChange={e => setNewTodoTitle(e.target.value)}
+					value={newTodoTitle}
+					/>
+					<button className="btn btn-success mb">Create</button>
+				</div>
+			</form>
 
-		<hr />
-		{todos.length > 0 && (
-				<>
-					<ul className="todolist">
-						{unfinishedTodos.map((todo, index) => (
-							<TodoListItem todo={todo} key={index} />
-						) )}
-					</ul>
+			<hr />
+			{todos.length > 0 && (
+					<>
+						<ul className="todolist">
+							{unfinishedTodos.map((todo, index) => (
+								<TodoListItem
+									onToggle={toggleTodo}
+									onDelete={deleteTodo}
+									todo={todo}
+									key={index}
+								/>
+							) )}
+						</ul>
 
-					<ul className="todolist">
-						{finishedTodos.map((todo, index) => (
-							<TodoListItem todo={todo} key={index} />
-						) )}
-					</ul>
+						<ul className="todolist">
+							{finishedTodos.map((todo, index) => (
+								<TodoListItem
+									onToggle={toggleTodo}
+									onDelete={deleteTodo}
+									todo={todo}
+									key={index}
+								/>
+							) )}
+						</ul>
 
-					<p className="status">
-						{finishedTodos.length} of {todos.length} todos completed
-					</p>
-				</>
-			)}
-		{todos.length === 0 && (<p className='text text-danger'>Empty todos, pls create moar!</p>)}
+						<TodoCounter finished={finishedTodos.length} total={todos.length} />
+					</>
+				)}
+			{todos.length === 0 && (<p className='text text-danger'>Empty todos, pls create moar!</p>)}
 
 		</div>
 		)
--- 02-react-simple-todos-2/src/assets/components/TodoListItem.tsx
+++ 02-react-simple-todos-2/src/assets/components/TodoListItem.tsx
@@ -3,23 +3,27 @@ import {Todo} from '../types/index'
 
 interface IProps {
 	todo: Todo
+	onDelete: (todoToDelete: Todo) => void
+	onToggle: (todo: Todo) => void
 }
 
-const TodoListItem: React.FC<IProps> = ({ todo }) => {
+const TodoListItem: React.FC<IProps> = ({ todo, onToggle }) => {
+
+
 	return (
 		<li className={todo.completed ? 'done' : ''}>
 			<span className="todo-title">
 				{todo.title}
 			</span>
 
-			{/* <span className="ms-1">
-				<span className="todo-toggle" onClick={() => toggleTodo(todo)} role="button">
+			<span className="ms-1">
+				<span className="todo-toggle" onClick={() => onToggle(todo)} role="button">
 					{todo.completed ? '☑️' : '✅'}
 				</span>
-				<span className="todo-delete" onClick={() => deleteTodo(todo)} role="button">
+				{/* <span className="todo-delete" onClick={() => deleteTodo(todo)} role="button">
 					🗑️
-				</span>
-			</span> */}
+				</span> */}
+			</span>
 		</li>
 	)
 }
