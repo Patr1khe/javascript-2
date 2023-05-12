57f77f12bba90c8e6793a3468bf27aba97e173a2
Patrik He <patrikswgit@gmail.com>
2023-05-12 11:19:34 +0200

    fetch data from api

---
+++ 04-fetch/src/App.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import './assets/scss/App.scss'
+
+interface IResource {
+	id: number
+	title: string
+}
+
+
+function App() {
+	const [resource, setResource] = useState('')
+	const [data, setData] = useState<IResource[]>([])
+
+	useEffect(() => {
+		if (!resource) {
+			return
+		}
+		const fetchData = async () => {
+			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+				const payload = await res.json()
+				setData(payload)
+		}
+		fetchData()
+	}, [resource])
+
+	return (
+		<div className="container">
+			<h1 className="mb-3">Fetch</h1>
+
+			<div className="d-flex justify-content-between">
+				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
+				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
+				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
+				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+			</div>
+
+			{resource && (
+				<>
+					<h2>{resource}</h2>
+					<p>There are {data.length} {resource}.</p>
+					<ol>
+						{data.map(item => (
+							<li key={item.id}>{item.title}</li>
+						))}
+					</ol>
+				</>
+			)}
+		</div>
+	)
+}
+
+export default App
