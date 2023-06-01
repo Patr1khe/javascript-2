82e5854d4651087af98c61b1f44b1272bbc9160c
Patrik He <patrikswgit@gmail.com>
2023-06-01 09:00:47 +0200

    added show loading, hide any pervious weather when searching.
     Catch any error when fetching weather data.
    
      Don't allow searching with less than 3 characters.
    
       Fixed getCurrentWeather fetch example.
    
       Show weather conditions using icons.
    
       Show weather conditions using icons
    
        Show weather freshness.
    
        Show banner depending on daytime or nighttime.

--- 05-react-weather/src/App.tsx
+++ 05-react-weather/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Forecast from './components/Forecast'
 import SearchCity from './components/SearchCity'
 import { getCurrentWeather } from './services/owmapi'
@@ -7,11 +7,46 @@ import Airplane from './assets/images/747.svg'
 import './assets/scss/App.scss'
 
 function App() {
+	const [currentWeather, setCurrentWeather] = useState<ICurrentWeather|null>(null)
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<string|false>(false)
+
+	const handleSearch = async (location: string) => {
+		setCurrentWeather(null)
+		setError(false)
+		setLoading(true)
+
+		try {
+			// call API and ask for weather in `location`
+			const data = await getCurrentWeather(location)
+
+			// update `currentWeather`-state with the current weather
+			setCurrentWeather(data)
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (err: any) {
+			setError(err.message)
+
+		}
+
+		setLoading(false)
+	}
+
 	return (
 		<div id="app" className="container">
-			<SearchCity />
+			<SearchCity onSearch={handleSearch} />
 
-			<Forecast />
+			{error && (
+				<div className="alert alert-warning">
+					{error}
+				</div>
+			)}
+
+			{loading && (
+				<img src={Airplane} className="img-fluid py-5 w-100" />
+			)}
+
+			{currentWeather && <Forecast data={currentWeather} />}
 		</div>
 	)
 }
--- 05-react-weather/src/components/Forecast.tsx
+++ 05-react-weather/src/components/Forecast.tsx
@@ -1,40 +1,62 @@
 import React from 'react'
-import forecastBanner from '../assets/images/forecast-banner.png'
+import { ICurrentWeather } from '../types'
+import dayBanner from '../assets/images/day.svg'
+import nightBanner from '../assets/images/night.svg'
 
-const Forecast = () => {
+interface IProps {
+	data: ICurrentWeather
+}
+
+const Forecast: React.FC<IProps> = ({ data }) => {
+	const now = Math.round(Date.now() / 1000)
+	const banner = now > data.sys.sunrise && now < data.sys.sunset
+		? dayBanner
+		: nightBanner
+
+	const freshness = new Date(data.dt * 1000).toLocaleString()
+
 	return (
 		<div id="forecast">
 			<div className="card">
 
-				<img src={forecastBanner} className="card-img-top" alt="Daytime, nighttime, daytime, nighttime"/>
+				<img src={banner} className="card-img-top" alt="Daytime, nighttime, daytime, nighttime"/>
 
 				<div className="card-body">
 					<h5 className="card-title" id="location">
-						<span id="city">CITY</span>,
-						<span id="country">COUNTRY</span>
+						<span id="city">{data.name}</span>,
+						<span id="country">{data.sys.country}</span>
 					</h5>
 					<p className="temp">
-						<span id="temperature">TEMP</span>
+						<span id="temperature">{data.main.temp}</span>
 						&deg;C
 					</p>
 					<p className="humidity">
-						<span id="humidity">HUMIDITY</span> % humidity
+						<span id="humidity">{data.main.humidity}</span> % humidity
 					</p>
 					<p className="wind">
-						<span id="windspeed">WIND_SPEED</span> m/s
+						<span id="windspeed">{data.wind.speed}</span> m/s {data.wind.deg}&deg;
 					</p>
 
-					{/*
 					<ul className="conditions">
-						<li><img src="" title="CONDITION_MAIN" alt="CONDITION_MAIN">CONDITION_DESCRIPTION</li>
+						{data.weather.map(condition => {
+							const weatherIconUrl = `https://openweathermap.org/img/wn/${condition.icon}@2x.png`
+
+							return (
+								<li key={condition.id}>
+									<img
+										src={weatherIconUrl}
+										title={condition.main}
+										alt={condition.main}
+									/>
+									{condition.description}
+								</li>
+							)
+						})}
 					</ul>
 
 					<p className="text-muted small">
-						<span>
-							1970-01-01 13:37:00
-						</span>
+						<span>{freshness}</span>
 					</p>
-					*/}
 				</div>
 
 			</div>
